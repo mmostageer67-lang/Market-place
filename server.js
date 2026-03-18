d4f26c41ef8e778f5cf83bdcfc328f251aa1ab60
mahmoud mostageer <mmostageer67@gmail.com>
2026-03-18 15:01:10 +0200

    Market place

---
+++ server.js
@@ -0,0 +1,19 @@
+const app =require('./app')
+const connectDB=require('./config/db')
+const dotenv=require('dotenv')
+dotenv.config()
+const PORT = process.env.PORT||3000
+const  startServer=async()=>
+{
+    try {
+        await connectDB()
+        app.listen(PORT,()=>
+        {
+            console.log(`the server is starting ${PORT}`)
+        })
+    } catch (error) {
+        console.log(`server fail to start:${error}`)
+    }
+}
+
+startServer()
