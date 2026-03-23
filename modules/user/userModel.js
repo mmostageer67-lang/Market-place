da5ee7efd7fb7da239a21a463cd9b60d48ceb2cf
mahmoud mostageer <mmostageer67@gmail.com>
2026-03-23 13:35:12 +0200

    create user model

---
+++ modules/user/userModel.js
@@ -0,0 +1,35 @@
+const mongoose =require('mongoose')
+const bcrypt=require('bcrypt')
+const userSchema=new mongoose.Schema({
+    name:
+    {
+        type:String,
+        required:[true,'user name is required.']
+    },
+    email:
+    {
+        type:String,
+        required:[true,'email is required.'],
+        unique:true,
+        match:[/^\S+@\S+\.\S+$/, 'Please use a valid email']
+    },
+    password:
+    {
+        type:String,
+        required:[true,'password is required.'],
+        select:false,
+        minlength:[6,'Password must be at least 6 characters']
+    },
+    role:
+    {
+        type:String,
+        enum:['admin','user'],
+        default:'user'
+    }
+},{timestamps:true})
+userSchema.pre('save',async function () {
+    if(!this.isModified('password'))return
+
+     this.password=await bcrypt.hash(this.password,10)
+})
+module.exports=mongoose.model('User',userSchema)
