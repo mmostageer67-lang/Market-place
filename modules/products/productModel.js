a3922478c809c217a657fb427c06ccc490f7591f
mahmoud mostageer <mmostageer67@gmail.com>
2026-03-27 20:26:25 +0200

    product schema

---
+++ modules/products/productModel.js
@@ -0,0 +1,36 @@
+const mongoose=require('mongoose')
+const productSchema=new mongoose.Schema ({
+name:{
+    type:String,
+    required:[true,'the name is required.'],
+    trim:true
+},
+price:
+{
+type:Number,
+required:[true,'price is required'],
+min: [0, 'Price must be positive']
+},
+description:
+{
+    type:String,
+},
+stock:
+{
+    type:Number,
+    default:0,
+    min: [0, 'stock must be positive']
+},
+category:
+{
+    type:String,
+    enum: ['electronics', 'clothes', 'food'],
+    required: true
+},
+createdBy:
+{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:'User'
+}
+},{timestamps:true})
+module.exports=mongoose.model('Product',productSchema)
