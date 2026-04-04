0df4b4de0ebc5431e082feb583d773707d42672a
mahmoud mostageer <mmostageer67@gmail.com>
2026-04-04 11:21:43 +0200

    creat cart model

---
+++ modules/cart/carModel.js
@@ -0,0 +1,26 @@
+const mongoose=require('mongoose')
+const cartSchema=new mongoose.Schema({
+user:{
+    type:mongoose.Schema.Types.ObjectId,
+    required:true,
+    ref:'User'
+},
+items:[{
+    product:{
+        type:mongoose.Schema.Types.ObjectId,
+        required:true,
+        ref:'Product'
+    },
+    price:{
+        type:Number,
+        required:true,
+        min:[0,"the price must be positive"]
+    },
+    quantity:{
+        type:Number,
+        required:true,
+        min:[1,"the quantity must be positive"]
+    }
+}]
+},{timestamps:true})
+module.exports=mongoose.model('cart',cartSchema)
