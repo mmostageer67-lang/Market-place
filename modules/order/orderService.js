2473b3a740125d176c15e134da1b51645fc7c736
mahmoud mostageer <mmostageer67@gmail.com>
2026-04-10 07:50:04 +0200

    create order service

---
+++ modules/order/orderService.js
@@ -0,0 +1,38 @@
+const Cart=require('../cart/cartModel')
+const Order=require('../order/orderModel')
+const createOrder=async (userId) => {
+
+        const cart=await Cart.findOne({ user: userId })
+       
+    if(!cart){
+            throw new Error("the Cart  not found!");
+            
+        }
+         if (cart.items.length === 0) {
+        throw new Error("Cart is empty")
+    }
+let totalPrice=0
+for(let item of cart.items)
+{
+    totalPrice+=item.price*item.quantity
+}
+const items=cart.items.map(item=>
+
+    ({
+product:item.product,
+price:item.price,
+quantity:item.quantity
+    })
+)
+const order=await Order.create({
+    user:userId,
+    items
+    ,
+    totalPrice
+})
+cart.items=[]
+ await cart.save()
+return order
+}
+
+module.exports={createOrder}
